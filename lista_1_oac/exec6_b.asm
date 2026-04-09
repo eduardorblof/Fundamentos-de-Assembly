e8de6a75aa4136314a346adb1c1f382829a31226
Eduardo Rabelo Feitosa <eduardo.rblof@gmail.com>
2026-04-09 19:01:11 -0300

    correção exec 6 item b

--- lista_1_oac/exec6_b.asm
+++ lista_1_oac/exec6_b.asm
@@ -1,19 +1,25 @@
 .data
-	quebra: .string "\n"
+quebra: .string "\n"
+
 .text
-main: 
-	addi x9, zero, 24
-	addi x10, zero, 42
+main:
+	addi t0, zero, 24   
+	addi t1, zero, 42   
+
 for:
-	bge x9, x10, exit
-	li a7, 1
-	add a0, zero, x9
+	bge t0, t1, exit
+
+	addi a7, zero, 1
+	add  a0, zero, t0
 	ecall
+
 	la a0, quebra
-	li a7, 4
+	addi a7, zero, 4
 	ecall
-	addi x9, x9, 3
-	jal for
+
+	addi t0, t0, 3
+	jal x0, for
+
 exit:
-	nop 
-	
+	addi a7, zero, 10
+	ecall
